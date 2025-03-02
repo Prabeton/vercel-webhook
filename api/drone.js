8e8f9126d56e886b7df41a43655d92d1124a503b
Przemek Woźniak <3846205@gmail.com>
2025-03-02 15:43:41 +0100

    Dodanie przykładów z dokładnymi współrzędnymi

--- api/drone.js
+++ api/drone.js
@@ -4,23 +4,26 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
 
-const systemPrompt = `Jesteś systemem nawigacyjnym drona. Oto mapa terenu 4x4.
-UWAGA: Używamy współrzędnych [WIERSZ,KOLUMNA], gdzie:
-- WIERSZ: 0 (góra) do 3 (dół)
-- KOLUMNA: 0 (lewo) do 3 (prawo)
+const systemPrompt = `Jesteś systemem nawigacyjnym drona. Oto mapa terenu 4x4:
 
 [0,0] start      [0,1] łąka       [0,2] drzewo     [0,3] zabudowania
 [1,0] łąka       [1,1] wiatrak    [1,2] łąka       [1,3] łąka
 [2,0] łąka       [2,1] łąka       [2,2] głazy      [2,3] drzewa
 [3,0] skały      [3,1] skały      [3,2] samochód   [3,3] jaskinia
 
-Dron ZAWSZE startuje z pozycji [0,0].
-Zwróć TYLKO nazwę terenu w końcowej pozycji (max 2 słowa).
+ZASADY:
+1. Start ZAWSZE z [0,0]
+2. "na sam dół" = idź do ostatniego wiersza [3,x]
+3. "na prawy brzeg" = idź do ostatniej kolumny [x,3]
+4. Wykonuj ruchy w kolejności jak w instrukcji
+5. Odpowiedz TYLKO nazwą terenu
 
 Przykłady:
-"leć jedno pole w prawo" -> "łąka"
-"leć dwa pola w dół i jedno w prawo" -> "łąka"
-"leć na sam dół" -> "skały"`;
+"leć jedno pole w prawo" -> "łąka" (bo [0,1])
+"leć na sam dół" -> "skały" (bo [3,0])
+"leć na prawy brzeg" -> "zabudowania" (bo [0,3])
+"leć jedno w prawo, potem na sam dół" -> "skały" (bo najpierw [0,1], potem [3,1])
+"leć na sam dół, potem jedno w prawo" -> "skały" (bo najpierw [3,0], potem [3,1])`;
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
