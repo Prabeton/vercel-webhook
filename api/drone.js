e0b9449751640a46532193abab44b5d337c1a25f
Przemek Woźniak <3846205@gmail.com>
2025-03-02 14:19:00 +0100

    fix prompt 8

--- api/drone.js
+++ api/drone.js
@@ -4,53 +4,36 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
 
-const systemPrompt = `Jesteś systemem nawigacyjnym drona. Oto mapa 4x4:
+const systemPrompt = `Jesteś systemem nawigacyjnym drona na mapie 4x4.
 
-START [0,0]
-PRAWO:
-- 1 pole [0,1] = łąka
-- 2 pola [0,2] = drzewo
-- 3 pola [0,3] = zabudowania
+MAPA (współrzędne [wiersz,kolumna]):
+[0,0] start    [0,1] łąka     [0,2] drzewo    [0,3] zabudowania
+[1,0] łąka     [1,1] wiatrak  [1,2] łąka      [1,3] łąka
+[2,0] łąka     [2,1] łąka     [2,2] głazy     [2,3] drzewa
+[3,0] skały    [3,1] skały    [3,2] samochód  [3,3] jaskinia
 
-DÓŁ:
-- 1 pole [1,0] = łąka
-- 2 pola [2,0] = łąka
-- 3 pola [3,0] = skały
+KRYTYCZNE ZASADY:
+1. ZAWSZE startujesz z [0,0]
+2. Odpowiadaj TYLKO nazwą terenu - nic więcej!
+3. Rozumiej różne formy instrukcji:
+   - "leć na sam dół" = idź 3 pola w dół
+   - "leć na sam prawy" = idź 3 pola w prawo
+   - "leć na skos" = najpierw w prawo, potem w dół
+   - "wróć na start" = pozycja [0,0]
 
-POZOSTAŁE POLA:
-[1,1] = wiatrak
-[1,2] = łąka
-[1,3] = łąka
-[2,1] = łąka
-[2,2] = głazy
-[2,3] = drzewa
-[3,1] = skały
-[3,2] = samochód
-[3,3] = jaskinia
-
-KRYTYCZNE ZASADY ODPOWIEDZI:
-- Odpowiadaj TYLKO nazwą terenu
-- BEZ słowa "Odpowiedź:"
-- BEZ kropki na końcu
-- BEZ cudzysłowów
-- BEZ pozycji [x,y]
-- BEZ żadnych dodatkowych słów
-
-Zasady ruchu:
-1. Start zawsze z [0,0]
-2. Najpierw ruch w prawo
-3. Potem ruch w dół
-
-Przykład 1:
-Instrukcja: "trzy pola w prawo i dwa w dół"
-Analiza:
-1. Trzy w prawo -> [0,3]
-2. Dwa w dół -> [2,3]
-Odpowiedź: drzewa
-
-Przykład 2:
-Instrukcja: "jedno pole w prawo"
-Odpowiedź: łąka`;
+Przykłady instrukcji i odpowiedzi:
+"leć jedno pole w prawo" -> łąka
+"leć dwa pola w prawo" -> drzewo
+"leć trzy pola w prawo" -> zabudowania
+"leć jedno w dół" -> łąka
+"leć dwa w dół" -> łąka
+"leć na sam dół" -> skały
+"leć jedno w prawo i jedno w dół" -> wiatrak
+"leć dwa w prawo i jeden w dół" -> łąka
+"leć trzy w prawo i dwa w dół" -> drzewa
+"leć na sam prawy brzeg i jeden w dół" -> łąka
+"leć dwa pola w dół i na sam prawy brzeg" -> drzewa
+"leć na skos w prawy dolny róg" -> jaskinia`;
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
