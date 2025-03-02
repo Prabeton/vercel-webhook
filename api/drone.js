f2ff55a09f10a874aca90feaaf9cdca3fdac59dd
Przemek Woźniak <3846205@gmail.com>
2025-03-02 14:20:45 +0100

    fix prompt 9

--- api/drone.js
+++ api/drone.js
@@ -6,34 +6,36 @@ const openai = new OpenAI({
 
 const systemPrompt = `Jesteś systemem nawigacyjnym drona na mapie 4x4.
 
-MAPA (współrzędne [wiersz,kolumna]):
-[0,0] start    [0,1] łąka     [0,2] drzewo    [0,3] zabudowania
-[1,0] łąka     [1,1] wiatrak  [1,2] łąka      [1,3] łąka
-[2,0] łąka     [2,1] łąka     [2,2] głazy     [2,3] drzewa
-[3,0] skały    [3,1] skały    [3,2] samochód  [3,3] jaskinia
+MAPA:
+[0,0] START -> [0,1] łąka -> [0,2] drzewo -> [0,3] zabudowania
+   ↓             ↓            ↓               ↓
+[1,0] łąka -> [1,1] wiatrak-> [1,2] łąka -> [1,3] łąka
+   ↓             ↓            ↓               ↓
+[2,0] łąka -> [2,1] łąka -> [2,2] głazy -> [2,3] drzewa
+   ↓             ↓            ↓               ↓
+[3,0] skały -> [3,1] skały -> [3,2] samochód-> [3,3] jaskinia
 
-KRYTYCZNE ZASADY:
-1. ZAWSZE startujesz z [0,0]
-2. Odpowiadaj TYLKO nazwą terenu - nic więcej!
-3. Rozumiej różne formy instrukcji:
-   - "leć na sam dół" = idź 3 pola w dół
-   - "leć na sam prawy" = idź 3 pola w prawo
-   - "leć na skos" = najpierw w prawo, potem w dół
-   - "wróć na start" = pozycja [0,0]
+ZASADY:
+1. START = pozycja [0,0]
+2. Odpowiadaj TYLKO nazwą terenu
+3. "na sam prawy" = 3 pola w prawo [x,3]
+4. "na sam dół" = 3 pola w dół [3,x]
+5. "na skos" = tyle samo pól w prawo i w dół
 
-Przykłady instrukcji i odpowiedzi:
-"leć jedno pole w prawo" -> łąka
-"leć dwa pola w prawo" -> drzewo
-"leć trzy pola w prawo" -> zabudowania
-"leć jedno w dół" -> łąka
-"leć dwa w dół" -> łąka
-"leć na sam dół" -> skały
-"leć jedno w prawo i jedno w dół" -> wiatrak
-"leć dwa w prawo i jeden w dół" -> łąka
-"leć trzy w prawo i dwa w dół" -> drzewa
-"leć na sam prawy brzeg i jeden w dół" -> łąka
-"leć dwa pola w dół i na sam prawy brzeg" -> drzewa
-"leć na skos w prawy dolny róg" -> jaskinia`;
+PRZYKŁADY:
+"leć na sam prawy brzeg i jeden w dół":
+1. najpierw na prawy brzeg (3 pola w prawo) -> [0,3] zabudowania
+2. potem jeden w dół -> [1,3] łąka
+Odpowiedź: łąka
+
+"leć dwa w dół i na prawy brzeg":
+1. najpierw dwa w dół -> [2,0] łąka
+2. potem na prawy brzeg -> [2,3] drzewa
+Odpowiedź: drzewa
+
+"leć na skos w prawy dolny róg":
+1. trzy w prawo i trzy w dół -> [3,3]
+Odpowiedź: jaskinia`;
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
