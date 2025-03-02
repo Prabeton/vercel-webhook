1900e0885667e3b29ce0c74ead54cde04f9e7314
Przemek Woźniak <3846205@gmail.com>
2025-03-02 14:12:45 +0100

    fix prompt 5

--- api/drone.js
+++ api/drone.js
@@ -6,42 +6,51 @@ const openai = new OpenAI({
 
 const systemPrompt = `Jesteś systemem nawigacyjnym drona, który lata nad mapą 4x4. Mapa wygląda następująco:
 
-POZYCJA [0,0] = start (PUNKT STARTOWY)
-POZYCJA [0,1] = łąka (1 pole w prawo od startu)
-POZYCJA [0,2] = drzewo (2 pola w prawo od startu)
-POZYCJA [0,3] = zabudowania (3 pola w prawo od startu)
+[WIERSZ 0]
+[0,0] = start
+[0,1] = łąka
+[0,2] = drzewo
+[0,3] = zabudowania
 
-POZYCJA [1,0] = łąka (1 pole w dół od startu)
-POZYCJA [1,1] = wiatrak (1 w dół, 1 w prawo od startu)
-POZYCJA [1,2] = łąka
-POZYCJA [1,3] = łąka
+[WIERSZ 1]
+[1,0] = łąka
+[1,1] = wiatrak
+[1,2] = łąka
+[1,3] = łąka
 
-POZYCJA [2,0] = łąka
-POZYCJA [2,1] = łąka
-POZYCJA [2,2] = głazy
-POZYCJA [2,3] = drzewa
+[WIERSZ 2]
+[2,0] = łąka
+[2,1] = łąka
+[2,2] = głazy
+[2,3] = drzewa
 
-POZYCJA [3,0] = skały
-POZYCJA [3,1] = skały
-POZYCJA [3,2] = samochód
-POZYCJA [3,3] = jaskinia
+[WIERSZ 3]
+[3,0] = skały
+[3,1] = skały
+[3,2] = samochód
+[3,3] = jaskinia
 
-WAŻNE ZASADY:
-1. Odpowiadaj TYLKO nazwą terenu, na którym znajduje się dron.
-2. Dron ZAWSZE startuje z pozycji [0,0].
-3. Wykonuj ruchy DOKŁADNIE w takiej kolejności, jak podano w instrukcji.
-4. Najpierw wykonaj WSZYSTKIE ruchy w jednym kierunku, potem w drugim.
-   Na przykład: "dwa pola w dół i jedno w prawo" oznacza:
-   - najpierw idź 2 pola w dół [2,0]
-   - potem 1 pole w prawo [2,1]
-   Końcowa pozycja to [2,1] gdzie jest "łąka"
+ZASADY PORUSZANIA SIĘ:
+1. Start ZAWSZE z [0,0]
+2. Ruch w prawo zwiększa drugą współrzędną
+3. Ruch w dół zwiększa pierwszą współrzędną
+4. Wykonuj ruchy DOKŁADNIE w kolejności z instrukcji
 
-Przykłady poprawnych odpowiedzi:
-"leć jedno pole w prawo" -> "łąka" (pozycja [0,1])
-"leć dwa pola w prawo" -> "drzewo" (pozycja [0,2])
-"leć jedno pole w prawo i jedno w dół" -> "wiatrak" (pozycja [1,1])
-"leć dwa pola w dół i jedno w prawo" -> "łąka" (pozycja [2,1])
-"leć na sam dół" -> "skały" (pozycja [3,0])`;
+Na przykład:
+"trzy pola w prawo i dwa w dół" oznacza:
+1. Najpierw trzy w prawo: [0,0] -> [0,3]
+2. Potem dwa w dół: [0,3] -> [2,3]
+Końcowa pozycja [2,3] = drzewa
+
+ODPOWIADAJ TYLKO NAZWĄ TERENU W KOŃCOWEJ POZYCJI.
+
+Przykłady:
+"leć jedno pole w prawo" -> "łąka" [0,1]
+"leć dwa pola w prawo" -> "drzewo" [0,2]
+"leć trzy pola w prawo" -> "zabudowania" [0,3]
+"leć jedno w dół" -> "łąka" [1,0]
+"leć dwa w dół" -> "łąka" [2,0]
+"leć trzy w dół" -> "skały" [3,0]`;
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
